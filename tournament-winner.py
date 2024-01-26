9fbb38f4bc3ef814e8e383df9c97be20c92d8b49
Marco <sanchez.junco@gmail.com>
2024-01-26 13:27:21 -0800

    removed going through the final scores rather kept track of best team along the way

--- tournament-winner.py
+++ tournament-winner.py
@@ -30,23 +30,55 @@ results = [0,0,1]
 #     print("The winner is ", tournWinner)
 #     return tournWinner
 
+# def tournamentWinner(competitions, results):
+#     wins = {}
+#     for faceOff in range(len(results)):
+#         if results[faceOff] == 0:
+#             winningTeam = competitions[faceOff][1]
+#             if winningTeam in wins:
+#                 wins[winningTeam] += 1
+#             else:
+#                 wins[winningTeam] = 1
+#         else:
+#             winningTeam = competitions[faceOff][0]
+#             if winningTeam in wins:
+#                 wins[winningTeam] += 1
+#             else:
+#                 wins[winningTeam] = 1
+#     tournWinner = max(wins, key=wins.get)
+#     return tournWinner
+
 def tournamentWinner(competitions, results):
     wins = {}
+    finalWinner = "no one"
     for faceOff in range(len(results)):
         if results[faceOff] == 0:
             winningTeam = competitions[faceOff][1]
             if winningTeam in wins:
-                wins[winningTeam] += 1
+                wins[winningTeam] += 3
             else:
-                wins[winningTeam] = 1
+                wins[winningTeam] = 3
         else:
             winningTeam = competitions[faceOff][0]
             if winningTeam in wins:
-                wins[winningTeam] += 1
+                wins[winningTeam] += 3
             else:
-                wins[winningTeam] = 1
-    tournWinner = max(wins, key=wins.get)
-    return tournWinner
+                wins[winningTeam] = 3
+        print("Most recent winner is ",winningTeam)
+        print(wins)
+        print(finalWinner)
+        if finalWinner == "no one":
+            finalWinner = winningTeam
+            print("First final winner is ", finalWinner)
+        else:
+            print("Current final winner name is ", finalWinner," and their score is ", wins[finalWinner])
+            print("Challenger winner name is ", winningTeam," and their score is ", wins[winningTeam])
+            if wins[winningTeam] > wins[finalWinner]:
+                print("final winner replaced by ", winningTeam)
+                finalWinner = winningTeam
+    # tournWinner = max(wins, key=wins.get)
+    return finalWinner
+
 
 answer = tournamentWinner(competitions, results)
 print(answer)
