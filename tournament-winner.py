2d0b319c6e982ec6a347f64a4f7a0ce695d5a5e5
Marco <sanchez.junco@gmail.com>
2024-01-26 12:57:04 -0800

    cleaned up

--- tournament-winner.py
+++ tournament-winner.py
@@ -6,28 +6,46 @@ competitions = [
 
 results = [0,0,1]
 
+# def tournamentWinner(competitions, results):
+#     wins = {}
+#     # print(wins)
+#     for faceOff in range(len(results)):
+#         # print(results[faceOff])
+#         if results[faceOff] == 0:
+#             winningTeam = competitions[faceOff][1]
+#             if winningTeam in wins:
+#                 wins[winningTeam] += 1
+#             else:
+#                 wins[winningTeam] = 1
+#             # print(wins)
+#         else:
+#             winningTeam = competitions[faceOff][0]
+#             if winningTeam in wins:
+#                 wins[winningTeam] += 1
+#             else:
+#                 wins[winningTeam] = 1
+#             # print(wins)
+    
+#     tournWinner = max(wins, key=wins.get)
+#     print("The winner is ", tournWinner)
+#     return tournWinner
+
 def tournamentWinner(competitions, results):
     wins = {}
-    # print(wins)
     for faceOff in range(len(results)):
-        # print(results[faceOff])
         if results[faceOff] == 0:
             winningTeam = competitions[faceOff][1]
             if winningTeam in wins:
                 wins[winningTeam] += 1
             else:
                 wins[winningTeam] = 1
-            # print(wins)
         else:
             winningTeam = competitions[faceOff][0]
             if winningTeam in wins:
                 wins[winningTeam] += 1
             else:
                 wins[winningTeam] = 1
-            # print(wins)
-    
     tournWinner = max(wins, key=wins.get)
-    print("The winner is ", tournWinner)
     return tournWinner
 
 answer = tournamentWinner(competitions, results)
