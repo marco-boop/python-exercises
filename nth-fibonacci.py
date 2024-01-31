2e1b90d330db05cd011459a0d888dbd6b0f6c6dc
Marco <sanchez.junco@gmail.com>
2024-01-31 09:36:49 -0800

    Works but not ideal

---
+++ nth-fibonacci.py
@@ -0,0 +1,16 @@
+n = 6
+
+def getNthFib(n):
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    if n > 1:
+        return(getNthFib(n-1) + getNthFib(n - 2))
+
+answer = getNthFib(n)
+print(answer)
+
+
