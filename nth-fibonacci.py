5926c703fd8ccee650d9cdaf9d268ad44f589b49
Marco <sanchez.junco@gmail.com>
2024-01-31 09:57:20 -0800

    array solution

--- nth-fibonacci.py
+++ nth-fibonacci.py
@@ -1,14 +1,36 @@
 n = 6
 
 def getNthFib(n):
-    if n <= 0:
-        return 0
-    if n == 1:
-        return 0
-    if n == 2:
-        return 1
-    if n > 1:
-        return(getNthFib(n-1) + getNthFib(n - 2))
+    fibArray = []
+    for i in range(n + 1):
+        if i == 0:
+            fibArray.append(0)
+        if i == 1:
+            fibArray.append(0)
+        if i == 2:
+            print("i is equal to 2")
+            fibArray.append(1)
+        if i > 2:
+            print("i is: ", i)
+            minusOne = fibArray[i -1]
+            print("minusOne is: ", minusOne)
+            minusTwo = fibArray[i-2]
+            print("minusTwo is: ", minusTwo)
+            fibArray.append(fibArray[i -1]+fibArray[i-2])
+    print(fibArray)
+    print(fibArray[n])
+    return fibArray[n]
+    
+
+# def getNthFib(n):
+#     if n <= 0:
+#         return 0
+#     if n == 1:
+#         return 0
+#     if n == 2:
+#         return 1
+#     if n > 1:
+#         return(getNthFib(n-1) + getNthFib(n - 2))
 
 answer = getNthFib(n)
 print(answer)
