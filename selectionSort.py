019d0138cb25205e734642e2f723e49888748052
Marco <sanchez.junco@gmail.com>
2024-02-01 10:03:40 -0800

    Struggled with this one

---
+++ selectionSort.py
@@ -0,0 +1,31 @@
+array = [5, -2, 2, -8, 3, -10, -6, -1, 2, -2, 9, 1, 1]
+
+def selectionSort(array):
+
+    def swap(array,a,b):
+        print("Gonna swap",array[a]," and ",array[b])
+        placeholder = array[a]
+        array[a] = array[b]
+        array[b] = placeholder
+        return array
+
+    for i in range(len(array)):
+        print("Outside: ",array[i],"----------------------")
+        smolIndex = i
+        for j in range(i + 1, len(array)):
+            if i != smolIndex:
+                if array[smolIndex] > array[j]:
+                    print("new smol is ",array[j]," at index ",j)
+                    smolIndex = j
+                    print("did smolIndex update?",smolIndex)
+            else:
+                if array[i] > array[j]:
+                    print("new smol is ",array[j]," at index ",j)
+                    smolIndex = j
+                    print("did smolIndex update?",smolIndex)
+        swap(array, i, smolIndex)
+
+    return array
+
+answer = selectionSort(array)
+print(answer)
