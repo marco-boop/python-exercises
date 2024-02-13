3d878a1e183d11f72533037cc432310db97cf950
Marco <sanchez.junco@gmail.com>
2024-02-13 11:20:01 -0800

    Better with pointers but still need to account for arrays that are all 1s

--- bestSeat.py
+++ bestSeat.py
@@ -1,39 +1,67 @@
-seats = [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1]
+# seats = [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1]
+seats = [1, 1, 1]
 
 def bestSeat(seats):
-    longest = 0
-    current = 0
-    endOfLongestIndex = 0
-    for i in range(len(seats)):
-        if seats[i] == 1:
-            current = 0
-        if seats[i] == 0:
-            current += 1
-        if current > longest:
-            longest = current
-            endOfLongestIndex = i
-    print("Longest segment is: ",longest)
-    print("endOfLongestIndex is: ",endOfLongestIndex)
-    if longest % 2 != 0:
-        middle = (longest // 2) + 1
-    else:
-        middle = longest //2
+    bestSeat = -1
+    longestSpace = 0
+    leftPointer = 0
+    rightPointer = 1
+    while rightPointer < len(seats):
+        print(leftPointer," and ", rightPointer)
+        if seats[leftPointer] == 1 and seats[rightPointer] == 1:
+            currentDistance = rightPointer - leftPointer
+            if currentDistance > longestSpace:
+                longestSpace = currentDistance
+                bestSeat = (leftPointer + rightPointer) // 2
+            leftPointer = rightPointer
+            rightPointer = leftPointer + 1
+        else:
+            rightPointer += 1
+    print("best seat at the end", bestSeat)
+    return bestSeat
 
-    print("MIddle is: ", middle)
 
-    print("Is this the answer: ",endOfLongestIndex - middle + 1)
+# def bestSeat(seats):
+#     longest = 0
+#     current = 0
+#     endOfLongestIndex = 0
+#     currentStart = None
+#     startoflongest = None
+#     for i in range(len(seats)):
+#         if seats[i] == 1:
+#             current = 0
+#             currentStart = i + 1
+#         if seats[i] == 0:
+#             current += 1
+#             if currentStart is None:
+#                 currentStart = i
+#         if current > longest:
+#             longest = current
+#             endOfLongestIndex = i
+#             startoflongest = currentStart
+#     print("Longest segment is: ",longest)
+#     print("endOfLongestIndex is: ",endOfLongestIndex)
+#     print("startoflongest is: ",startoflongest)
+#     if longest % 2 != 0:
+#         middle = (longest // 2) + 1
+#     else:
+#         middle = longest //2
 
-    if endOfLongestIndex == 0:
-        return -1
-    elif longest == 2:
-            selectedSeat = endOfLongestIndex - 1
-    else:
-        if longest % 2 == 0:
-            selectedSeat = endOfLongestIndex - middle
-        else:        
-            selectedSeat = endOfLongestIndex - middle + 1
+#     print("MIddle is: ", middle)
+
+#     print("Is this the answer: ",endOfLongestIndex - middle + 1)
+
+#     if endOfLongestIndex == 0:
+#         return -1
+#     elif longest == 2:
+#             selectedSeat = endOfLongestIndex - 1
+#     else:
+#         if longest % 2 == 0:
+#             selectedSeat = endOfLongestIndex - middle
+#         else:        
+#             selectedSeat = endOfLongestIndex - middle + 1
     
-    return selectedSeat
+#     return selectedSeat
 
 answer = bestSeat(seats)
 print(answer)
